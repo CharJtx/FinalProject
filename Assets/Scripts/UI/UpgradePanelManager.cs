be9b114c3518d52e66d283e9e2f50f734493ff89
CharJtx <147318897+CharJtx@users.noreply.github.com>
2024-08-07 11:03:50 +0100

    Upgrade panel
    1. Add a UI skin
    2. Change Upgrade panel generate method

--- Assets/Scripts/UI/UpgradePanelManager.cs
+++ Assets/Scripts/UI/UpgradePanelManager.cs
@@ -5,34 +5,103 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using Newtonsoft.Json;
+using TMPro;
 
 
 public class UpgradePanelManager : MonoBehaviour
 {
-    public GameObject upgradePanel;
-    public Button buttonPrefab;
-    private List<Button> upgradeButtons = new List<Button>();
+    //private List<Button> upgradeButtons = new List<Button>();
     private int upgradeButtonCount = 3;
     private GameObject player;
-    private List<UpgradeOption> upgradeOptions;
+    private List<UpgradeOption> upgradeOptions = new List<UpgradeOption>();
+    private List<UpgradeOption> selectedUpgradOptions = new List<UpgradeOption>();
+    private List<int> valueList = new List<int>();
+
+    private Rect windowRect;
+    public GUISkin guiskin;
+    private bool showWindow = false;
+    private int screenHight;
+    private int screenWidth;
+    private int panelSpacing = 20;
+    private float panelWidth;
+    private float panelHeight;
+    private GUIStyle textBoxStyle;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        upgradePanel.SetActive(false);
+        //upgradePanel.SetActive(false);
+        screenHight = Screen.height;
+        screenWidth = Screen.width;
+        Debug.Log(screenHight);
+        Debug.Log(screenWidth);
+        screenHight = 500;
+        screenWidth = 800;
+        panelWidth = screenWidth - 2 * panelSpacing;
+        panelHeight = screenHight - 2 * panelSpacing;
+
+        windowRect = new Rect(panelSpacing, panelSpacing, panelWidth, panelHeight);
         LoadUpgradeOptions();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
+    private void OnGUI()
+    {
+        textBoxStyle = new GUIStyle(GUI.skin.box);
+        textBoxStyle.alignment = TextAnchor.MiddleCenter;
+        textBoxStyle.wordWrap = true;
+        GUI.skin = guiskin;
+        if (showWindow)
+        {
+            windowRect = GUI.Window(0, windowRect, DoMyWindow, "UPDATE OPTIONS");
+        }
+    }
+
+    void DoMyWindow(int windowID)
+    {
+        for (int i = 0; i < upgradeButtonCount; i++)
+        {
+            UpgradeOption option = selectedUpgradOptions[i];
+            int randomValue = valueList[i];
+            string buttonDescription = option.description.Replace("{Num}", randomValue.ToString());
+            string buttonText = option.name;
+            float spacing = 20;
+
+            float Rank = panelWidth / upgradeButtonCount;
+            float buttonWidth = Rank - 3*spacing;
+            float buttonHeight = 30;
+
+            float buttonY = screenHight - buttonHeight - 2* spacing -panelSpacing - 10;
+            float buttonX = panelSpacing + spacing + Rank * i;
+
+            float textBoxHeight = screenHight - 3*spacing - 2 * panelSpacing - buttonHeight - 20;
+            float textBoxWidth = buttonWidth;
+
+            float textY = panelSpacing + spacing;
+            float textX = buttonX;
+
+            
+            Rect textBoxRect = new Rect(textX, textY, textBoxWidth, textBoxHeight);
+            GUI.TextArea(textBoxRect, buttonDescription, textBoxStyle);
+
+            if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), buttonText))
+            {
+                OnUpgradeButtonClicked(option, randomValue);
+            }
+
+            
+            
+        }
+    }
     void LoadUpgradeOptions()
     {
-        string filePath = Path.Combine(Application.streamingAssetsPath, "upgradeOptions").Replace("\\","/");
+        string filePath = Path.Combine(Application.streamingAssetsPath, "upgradeOptions").Replace("\\", "/");
         Debug.Log(filePath);
         if (!File.Exists(filePath))
         {
@@ -49,49 +118,39 @@ public class UpgradePanelManager : MonoBehaviour
 
     public void ShowUpgradeOptions()
     {
-        foreach (Button button in upgradeButtons)
-        {
-            Destroy(button.gameObject);
-        }
-        upgradeButtons.Clear();
+        selectedUpgradOptions.Clear();
+        valueList.Clear();
 
-        Time.timeScale = 0;
-
-        string[] upgradeDescriptionArray = new string[upgradeButtonCount];
-        string[] upgradeNameArray = new string[upgradeButtonCount];
+        System.Random random = new System.Random();
 
         for (int i = 0; i < upgradeButtonCount; i++)
         {
-            Button newButton = Instantiate(buttonPrefab, upgradePanel.transform);
-            newButton.name = "Button" + i;
-
             UpgradeOption option = upgradeOptions[Random.Range(0, upgradeOptions.Count)];
-            string buttonText = option.name;
+            selectedUpgradOptions.Add(option);
+
             int randomValue = 0;
             if (option.type == "value")
             {
                 randomValue = Random.Range(10, 100);
             }
-            else if (option.type == "health")
+            else if (option.type == "percentage")
             {
                 randomValue = Random.Range(1, 50);
             }
-            string buttonDescription = option.description.Replace("{percentage}", randomValue.ToString());
 
-            //newButton.GetComponentInChildren<Text>().text = buttonDescription;
-            newButton.onClick.AddListener(() => OnUpgradeButtonClicked(option, randomValue));
-            upgradeButtons.Add(newButton);
-
+            valueList.Add(randomValue);
         }
 
-        upgradePanel.SetActive(true);
+        Time.timeScale = 0;
+        showWindow = true;
     }
 
     void OnUpgradeButtonClicked(UpgradeOption option, int value)
     {
         performUpgradeMethod(option, value);
-        upgradePanel.SetActive(false);
-        Time.timeScale = 1;
+        showWindow = false;
+        //upgradePanel.SetActive(false);
+
     }
 
     void performUpgradeMethod(UpgradeOption option, int value)
@@ -101,7 +160,9 @@ public class UpgradePanelManager : MonoBehaviour
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Component[] components = MyTools.GetComponentByName(player, componentName);
         MyTools.CallMethod(components, methodName, value);
-
+        Time.timeScale = 1;
     }
+
+
 
 }
